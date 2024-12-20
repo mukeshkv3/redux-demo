60f4a530f41653c2733fe27fda5472a97949817a
mukeshkv3 <mmuki72@gmail.com>
2024-12-20 14:29:15 +0530

    Implement office management slice with async thunk for fetching offices

--- src/redux-with-thunk/OfficeSlice.jsx
+++ src/redux-with-thunk/OfficeSlice.jsx
@@ -1,15 +1,35 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { Axios } from 'axios';
+import { Axios } from 'axios'
 
 const initialState = {
-
+    offices: [], isLoading: false, hasError: null
 }
 
+export const getOffices = createAsyncThunk("offices/getOffices", async () => {
+    const response = await Axios.get("http://localhost:5050/offices")
+    return response.data
+})
+
 const OfficeSlice = createSlice({
-  name: second,
-  initialState,
-  reducers: {}
-});
+  name: 'office',
+  initialState : initialState,
+  reducers: {},
+  extraReducers: {
+    [getOffices.pending]: (state) => {
+      state.isLoading = true
+      state.hasError = null
+    },
+    [getOffices.fulfilled]: (state, action) => {
+      state.offices = action.payload
+      state.isLoading = false
+      state.hasError = null
+    },
+    [getOffices.rejected]: (state, action) => {
+      state.isLoading = false
+      state.hasError = action.error
+    }
+  }
+})
 
 export const {} = OfficeSlice.actions
 
--- src/redux-with-thunk/Store.jsx
+++ src/redux-with-thunk/Store.jsx
@@ -1,10 +1,11 @@
-import {  applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { configureStore } from '@reduxjs/toolkit'
+import OfficeSlice from './OfficeSlice'
 
-const store = configureStore(
-    rootReducer,
-    applyMiddleware(thunk)
-);
+const store = configureStore({
+    office: OfficeSlice.reducer
+},
+)
 
-export default store;
+export default store
